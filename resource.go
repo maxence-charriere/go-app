2f533f545c2ee1b85bc268735be74bce9f376719
Maxence Charriere <charriere@outlook.com>
2016-11-05 21:53:38 -0700

    ResourcePath CSS method implementation

--- resource.go
+++ resource.go
@@ -1,24 +1,61 @@
 package app
 
 import (
+	"os"
 	"path/filepath"
+
+	"io/ioutil"
+
+	"strings"
+
+	"github.com/murlokswarm/log"
 )
 
 // ResourcePath represents the path of the app resource directory.
 type ResourcePath string
 
 // Path is a convenient method that cast a ResourcePath into a string.
-func (m ResourcePath) Path() string {
-	return string(m)
+func (r ResourcePath) Path() string {
+	return string(r)
 }
 
 // Join joins any number of path elements into a single path, adding a
 // Separator if necessary.
 // It calls the Join function from package path/filepath with the resource
 // location as the first element.
-func (m ResourcePath) Join(elems ...string) string {
-	elems = append([]string{m.Path()}, elems...)
+func (r ResourcePath) Join(elems ...string) string {
+	elems = append([]string{r.Path()}, elems...)
 	return filepath.Join(elems...)
+}
+
+// CSS returns a slice containing the css filenames of the css directory.
+func (r ResourcePath) CSS() (css []string) {
+	cssPath := r.Join("css")
+
+	info, err := os.Stat(cssPath)
+	if err != nil {
+		log.Warnf("%v doesn't exists", cssPath)
+		return
+	}
+
+	if !info.IsDir() {
+		log.Errorf("%v is not a directory", cssPath)
+		return
+	}
+
+	files, _ := ioutil.ReadDir(cssPath)
+
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+
+		if strings.HasSuffix(f.Name(), ".css") {
+			css = append(css, f.Name())
+		}
+	}
+
+	return
 }
 
 // Resources returns the path of the app resource directory.
--- resource_test.go
+++ resource_test.go
@@ -1,13 +1,53 @@
 package app
 
 import "testing"
+import "os"
+import "path/filepath"
 
-func TestResourceLocationJoin(t *testing.T) {
+func TestResourcePathJoin(t *testing.T) {
 	l := ResourcePath("resources")
 
 	if j := l.Join("css"); j != "resources/css" {
 		t.Error("j should be resources/css:", j)
 	}
+}
+
+func TestResourcePathCSS(t *testing.T) {
+	r := ResourcePath("resources")
+	cssPath := r.Join("css")
+
+	if err := os.MkdirAll(cssPath, os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(Resources().Path())
+
+	os.Create(filepath.Join(cssPath, "foo.css"))
+	os.Create(filepath.Join(cssPath, "hello"))
+	os.Create(filepath.Join(cssPath, "world.txt"))
+	os.Mkdir(filepath.Join(cssPath, "bar"), os.ModePerm)
+
+	cssFilenames := r.CSS()
+
+	if l := len(cssFilenames); l != 1 {
+		t.Error("cssFilenames should have 1 element:", l)
+	}
+
+	if cssFilenames[0] != "foo.css" {
+		t.Error("cssFilenames[0] should be foo.css:", cssFilenames[0])
+	}
+}
+
+func TestResourcePathCSSError(t *testing.T) {
+	// No css directory.
+	r := ResourcePath("resources")
+	r.CSS()
+
+	// css is not a directory.
+	cssPath := r.Join("css")
+	os.Mkdir(r.Path(), os.ModePerm)
+	os.Create(cssPath)
+	defer os.RemoveAll(Resources().Path())
+	r.CSS()
 }
 
 func TestResources(t *testing.T) {
