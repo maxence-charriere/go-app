6e09ab726de4555a8ce5914674d7c914f9f7729b
Maxence Charriere <charriere@outlook.com>
2017-08-17 21:45:57 -0700

    Ui element partial

--- ui.go
+++ ui.go
@@ -1,12 +1,58 @@
 package app
 
-type Window interface{}
+import "github.com/google/uuid"
+
+// Element is the interface that describes an app element.
+type Element interface {
+	// ID returns the element identifier.
+	ID() uuid.UUID
+}
+
+type Navigator interface {
+	Element
+
+	// Navigate navigates to the specified URL.
+	// Calls with an URL which contains a component name will load the named
+	// component.
+	// e.g. /hello
+	Navigate(url string) error
+
+	CanPrevious() bool
+
+	// Previous navigates to the previous page.
+	Previous()
+
+	CanNext() bool
+
+	// Next navigates to the next page.
+	Next()
+}
+
+type Window interface {
+	Navigator
+
+	Position() (x, y float64)
+
+	Move(x, y float64)
+
+	Size() (width, height float64)
+
+	Resize(width, height float64)
+
+	Focus()
+
+	Close()
+}
 
 type WindowConfig struct{}
 
-type MenuBar interface{}
+type MenuBar interface {
+	Navigator
+}
 
 type Dock interface {
+	Navigator
+
 	SetIcon(name string)
 
 	SetBadge(v interface{})
