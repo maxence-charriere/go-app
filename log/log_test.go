c62325c47b25275facdb0ee3a7d2bd57fc7d9a68
Maxence Charriere <charriere@outlook.com>
2017-09-30 16:11:17 -0700

    Return line for logs with format

--- log/log_test.go
+++ log/log_test.go
@@ -6,13 +6,13 @@ func TestLogger(t *testing.T) {
 	l := Logger{}
 
 	l.Log("hello", "world")
-	l.Logf("%s %s\n", "hello", "world")
+	l.Logf("%s %s", "hello", "world")
 
 	l.Debug = true
 
 	l.Log("hello", "world")
-	l.Logf("%s %s\n", "hello", "world")
+	l.Logf("%s %s", "hello", "world")
 
 	l.Error("hello", "world")
-	l.Errorf("%s %s\n", "hello", "world")
+	l.Errorf("%s %s", "hello", "world")
 }
