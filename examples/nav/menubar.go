8857587001138a263dca3f6beca0f3ebb186f929
Maxence Charriere <charriere@outlook.com>
2017-11-26 12:35:22 +0800

    Fix build

--- examples/nav/menubar.go
+++ examples/nav/menubar.go
@@ -1,26 +1,26 @@
 package main
 
-import (
-	"github.com/murlokswarm/app"
-	"github.com/murlokswarm/app/markup"
-)
+// import (
+// 	"github.com/murlokswarm/app"
+// 	"github.com/murlokswarm/app/markup"
+// )
 
-// MenuBar is the component that define the menu bar.
-type MenuBar markup.ZeroCompo
+// // MenuBar is the component that define the menu bar.
+// type MenuBar markup.ZeroCompo
 
-// Render returns return the HTML describing the menu bar.
-func (m *MenuBar) Render() string {
-	return `
-<menu>
-	<menu label="app">
-		<menuitem label="Close" selector="performClose:" shortcut="meta+w" />
-		<menuitem label="Quit" selector="terminate:" shortcut="meta+q" /> 
-	</menu>
-</menu>
-	`
-}
+// // Render returns return the HTML describing the menu bar.
+// func (m *MenuBar) Render() string {
+// 	return `
+// <menu>
+// 	<menu label="app">
+// 		<menuitem label="Close" selector="performClose:" shortcut="meta+w" />
+// 		<menuitem label="Quit" selector="terminate:" shortcut="meta+q" />
+// 	</menu>
+// </menu>
+// 	`
+// }
 
-// /!\ Import the component. Required to use a component.
-func init() {
-	app.Import(&MenuBar{})
-}
+// // /!\ Import the component. Required to use a component.
+// func init() {
+// 	app.Import(&MenuBar{})
+// }
