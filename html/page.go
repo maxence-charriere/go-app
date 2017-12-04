40e6c38bd7814764bf98df37941863bc7228c4da
Maxence Charriere <charriere@outlook.com>
2017-12-03 19:23:28 -0800

    Save work

---
+++ html/page.go
@@ -0,0 +1,22 @@
+package html
+
+// PageConfig is the struct that describes a page.
+type PageConfig struct {
+	// The app.js script.
+	AppJS string
+
+	// The default component rendering.
+	DefaultComponent string
+
+	// The CSS filenames to include.
+	CSS []string
+
+	// The javascript filenames to include.
+	Javasripts []string
+}
+
+func Page(config PageConfig) (page string, err error) {
+	tmpl := ``
+
+	return
+}
