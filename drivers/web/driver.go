f444079eb71a20e54fe1beaf76fb499ed2b66c03
Maxence Charriere <charriere@outlook.com>
2018-02-27 01:25:55 -0800

    server resources and not found

--- drivers/web/driver.go
+++ drivers/web/driver.go
@@ -3,6 +3,8 @@ package web
 
 import (
 	"net/http"
+
+	"github.com/murlokswarm/app"
 )
 
 // Driver is an app.Driver implementation for web.
@@ -11,9 +13,13 @@ type Driver struct {
 	DefaultURL string
 
 	// The URL of the component to load when a 404 errors occurs.
+	// Default is /web.NotFound
 	NotFoundURL string
 
 	// The server used to save request.
 	// Default is a server that listens on port 7042.
 	Server *http.Server
+
+	factory     app.Factory
+	fileHandler http.Handler
 }
--- drivers/web/server.go
+++ drivers/web/server.go
@@ -3,9 +3,16 @@
 package web
 
 import (
+	"bytes"
+	"context"
+	"html/template"
+	"net/http"
+	"os"
 	"path"
+	"path/filepath"
 
 	"github.com/murlokswarm/app"
+	"github.com/murlokswarm/app/html"
 )
 
 // Name satisfies the app.Driver interface.
@@ -15,7 +22,92 @@ func (d *Driver) Name() string {
 
 // Run satisfies the app.Driver interface.
 func (d *Driver) Run(f app.Factory) error {
-	panic("not implemented")
+	d.factory = f
+
+	if len(d.NotFoundURL) == 0 {
+		d.NotFoundURL = "web.NotFound"
+	}
+
+	if d.Server == nil {
+		d.Server = &http.Server{
+			Addr: ":7042",
+		}
+	}
+
+	d.fileHandler = http.FileServer(http.Dir("resources"))
+	d.Server.Handler = d
+	// http.Handle("/", d)
+
+	errC := make(chan error)
+	go func() {
+		err := d.Server.ListenAndServe()
+		if err == http.ErrServerClosed {
+			err = nil
+		}
+		errC <- err
+	}()
+
+	return <-errC
+}
+
+// ServeHTTP is the http.Handler that route wether to serve a page or a
+// resource.
+func (d *Driver) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	if len(req.URL.Path) == 0 || req.URL.Path == "/" {
+		req.URL.Path = d.DefaultURL
+	}
+
+	if compoName := app.ComponentNameFromURL(req.URL); d.factory.Registered(compoName) {
+		d.handleComponent(res, req)
+		return
+	}
+
+	filename := filepath.Join("resources", req.URL.Path)
+	if fi, err := os.Stat(filename); err != nil || fi.IsDir() {
+		req.URL.Path = d.NotFoundURL
+		d.handleNotFound(res, req)
+		return
+	}
+
+	d.fileHandler.ServeHTTP(res, req)
+}
+
+func (d *Driver) handleComponent(res http.ResponseWriter, req *http.Request) {
+	compoName := app.ComponentNameFromURL(req.URL)
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte(compoName))
+}
+
+func (d *Driver) handleNotFound(res http.ResponseWriter, req *http.Request) {
+	compo, err := d.factory.New(app.ComponentNameFromURL(req.URL))
+	if err != nil {
+		http.NotFound(res, req)
+		return
+	}
+
+	markup := html.NewMarkup(d.factory)
+
+	var root app.Tag
+	if root, err = markup.Mount(compo); err != nil {
+		http.NotFound(res, req)
+		return
+	}
+
+	var b bytes.Buffer
+	enc := html.NewEncoder(&b, markup)
+
+	if err = enc.Encode(root); err != nil {
+		http.NotFound(res, req)
+		return
+	}
+
+	page := html.NewPage(html.PageConfig{
+		Title:            "not found",
+		DefaultComponent: template.HTML(b.String()),
+	})
+
+	res.WriteHeader(http.StatusNotFound)
+	res.Write([]byte(page))
 }
 
 // AppName satisfies the app.Driver interface.
@@ -46,12 +138,12 @@ func (d *Driver) NewContextMenu(c app.MenuConfig) (app.Menu, error) {
 
 // Render satisfies the app.Driver interface.
 func (d *Driver) Render(c app.Component) error {
-	panic("not implemented")
+	return app.NewErrNotSupported("render")
 }
 
 // ElementByComponent satisfies the app.Driver interface.
 func (d *Driver) ElementByComponent(c app.Component) (app.ElementWithComponent, error) {
-	panic("not implemented")
+	return nil, app.NewErrNotSupported("element by component")
 }
 
 // NewFilePanel satisfies the app.Driver interface.
@@ -87,4 +179,9 @@ func (d *Driver) Dock() app.DockTile {
 // CallOnUIGoroutine satisfies the app.Driver interface.
 func (d *Driver) CallOnUIGoroutine(f func()) {
 	panic("not implemented")
+}
+
+// Close shutdown the server.
+func (d *Driver) Close() {
+	d.Server.Shutdown(context.Background())
 }
