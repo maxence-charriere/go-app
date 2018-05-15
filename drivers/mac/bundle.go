7cd11380af1517d63d368fe784a4549b848a980f
Maxence Charriere <charriere@outlook.com>
2018-05-14 23:56:17 -0700

    add mac categories (#132)

--- drivers/mac/bundle.go
+++ drivers/mac/bundle.go
@@ -38,8 +38,7 @@ type Bundle struct {
 	Role Role
 
 	// The application category.
-	// See https://developer.apple.com/library/content/documentation/General/Reference/InfoPlistKeyReference/Articles/LaunchServicesKeys.html#//apple_ref/doc/uid/TP40009250-SW8.
-	Category string
+	Category Category
 
 	// Reports whether the app runs in sandbox mode.
 	Sandbox bool
@@ -109,4 +108,53 @@ const (
 	FileNoAccess        FileAccess = ""
 	FileReadAccess      FileAccess = "read-only"
 	FileReadWriteAccess FileAccess = "read-write"
+)
+
+// Category represents the app style.
+// The App Store uses this string to determine the appropriate categorization
+// for the app.
+type Category string
+
+// Constants that enumerate application categories.
+const (
+	BusinessApp             Category = "public.app-category.business"
+	DeveloperToolsApp                = "public.app-category.developer-tools"
+	EducationApp                     = "public.app-category.education"
+	EntertainmentApp                 = "public.app-category.entertainment"
+	FinanceApp                       = "public.app-category.finance"
+	GamesApp                         = "public.app-category.games"
+	GraphicsAndDesignApp             = "public.app-category.graphics-design"
+	HealthcareAndFitnessApp          = "public.app-category.healthcare-fitness"
+	LifestyleApp                     = "public.app-category.lifestyle"
+	MedicalApp                       = "public.app-category.medical"
+	MusicApp                         = "public.app-category.music"
+	NewsApp                          = "public.app-category.news"
+	PhotographyApp                   = "public.app-category.photography"
+	ProductivityApp                  = "public.app-category.productivity"
+	ReferenceApp                     = "public.app-category.reference"
+	SocialNetworkingApp              = "public.app-category.social-networking"
+	SportsApp                        = "public.app-category.sports"
+	TravelApp                        = "public.app-category.travel"
+	UtilitiesApp                     = "public.app-category.utilities"
+	VideoApp                         = "public.app-category.video"
+	WeatherApp                       = "public.app-category.weather"
+	ActionGamesApp                   = "public.app-category.action-games"
+	AdventureGamesApp                = "public.app-category.adventure-games"
+	ArcadeGamesApp                   = "public.app-category.arcade-games"
+	BoardGamesApp                    = "public.app-category.board-games"
+	CardGamesApp                     = "public.app-category.card-games"
+	CasinoGamesApp                   = "public.app-category.casino-games"
+	DiceGamesApp                     = "public.app-category.dice-games"
+	EducationalGamesApp              = "public.app-category.educational-games"
+	FamilyGamesApp                   = "public.app-category.family-games"
+	KidsGamesApp                     = "public.app-category.kids-games"
+	MusicGamesApp                    = "public.app-category.music-games"
+	PuzzleGamesApp                   = "public.app-category.puzzle-games"
+	RacingGamesApp                   = "public.app-category.racing-games"
+	RolePlayingGamesApp              = "public.app-category.role-playing-games"
+	SimulationGamesApp               = "public.app-category.simulation-games"
+	SportsGamesApp                   = "public.app-category.sports-games"
+	StrategyGamesApp                 = "public.app-category.strategy-games"
+	TriviaGamesApp                   = "public.app-category.trivia-games"
+	WordGamesApp                     = "public.app-category.word-games"
 )
