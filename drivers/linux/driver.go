e8d7460d6a9b3904249694f5add0cecad3b19e87
Maxence Charriere <charriere@outlook.com>
2018-08-02 22:44:24 -0700

    cleanup linux

--- drivers/linux/driver.go
+++
@@ -1,9 +0,0 @@
-// +build linux
-
-// Package linux is the driver to be used for applications that will run on
-// Linux.
-package linux
-
-// Driver is the app.Driver implementation for Linux.
-type Driver struct {
-}
