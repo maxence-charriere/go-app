25c0464c663c2606d643bb16ea2363648764f37a
Maxence Charriere <charriere@outlook.com>
2018-08-27 01:47:20 -0700

    fix linux build

--- cmd/goapp/linux.go
+++ cmd/goapp/linux.go
@@ -2,6 +2,21 @@
 
 package main
 
+import (
+	"context"
+	"os"
+)
+
 func openCommand() string {
 	return "xdg-open"
+}
+
+func mac(ctx context.Context, args []string) {
+	printErr("you are not on MacOS!")
+	os.Exit(-1)
+}
+
+func win(ctx context.Context, args []string) {
+	printErr("you are not on Windows!")
+	os.Exit(-1)
 }
