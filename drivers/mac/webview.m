bb3e2b26d33836612e233a6219bb874bec72f742
Maxence Charriere <charriere@outlook.com>
2018-09-25 23:13:56 -0700

    fix pasteboard deprecation

--- drivers/mac/webview.m
+++ drivers/mac/webview.m
@@ -4,17 +4,39 @@
 
 @implementation AppWebView
 - (BOOL)prepareForDragOperation:(id<NSDraggingInfo>)sender {
-  NSPasteboard *pboard = [sender draggingPasteboard];
+  NSPasteboard *pasteboard = [sender draggingPasteboard];
+  NSMutableArray<NSString *> *filenames = [[NSMutableArray alloc] init];
+  NSArray<NSURL *> *url =
+      [pasteboard propertyListForType:NSPasteboardTypeURL];
 
-  if ([[pboard types] containsObject:NSFilenamesPboardType]) {
-    Driver *driver = [Driver current];
+  for (NSPasteboardItem *item in pasteboard.pasteboardItems) {
+    NSData *data = [item dataForType:NSPasteboardTypeFileURL];
+    if (data == nil) {
+      continue;
+    }
 
+    NSString *uString =
+        [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSURL *u = [[NSURL alloc] initWithString:uString];
+
+    u = u.filePathURL;
+    if (u == nil) {
+      continue;
+    }
+
+    u = u.absoluteURL;
+    [filenames addObject:u.path];
+  }
+
+  if (filenames.count != 0) {
     NSDictionary *in = @{
-      @"Filenames" : [pboard propertyListForType:NSFilenamesPboardType],
+      @"Filenames" : filenames,
     };
 
+    Driver *driver = [Driver current];
     [driver.goRPC call:@"driver.OnFileDrop" withInput:in onUI:YES];
   }
+
   return [super prepareForDragOperation:sender];
 }
 @end
