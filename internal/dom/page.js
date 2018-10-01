7e92899f6fd970774a45f94c6c8ac957b5726664
Maxence Charriere <charriere@outlook.com>
2018-09-30 22:32:40 -0700

    event source value (#206)

--- internal/dom/page.js
+++ internal/dom/page.js
@@ -338,6 +338,7 @@ function setPayloadSource(payload, elem) {
         'CompoID': elem.compoID,
         'ID': elem.id,
         'Class': elem.className,
-        'Data': elem.dataset
+        'Data': elem.dataset,
+        'Value': elem.value
     };
 }
