f73893f37d8eb88543d4ebe6f1093c113277d372
Maxence Charriere <charriere@outlook.com>
2018-11-30 01:40:58 -0800

    fix demo mac

--- examples/demo/main_darwin.go
+++ examples/demo/main_darwin.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"github.com/murlokswarm/app"
+	"github.com/murlokswarm/app/drivers/mac"
+)
 
 func mainDesktop() {
-	fmt.Println("MacOS")
+	app.Run(&mac.Driver{
+		URL: "/Hello",
+	})
 }
