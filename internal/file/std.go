e47fc206f192cc5a8990c88262e7d6a6c7845481
Maxence Charriere <charriere@outlook.com>
2018-12-02 00:39:33 -0800

    capture output

---
+++ internal/file/std.go
@@ -0,0 +1,80 @@
+package file
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"sync"
+)
+
+// CaptureOutput read from os.Stdout and os.Stderr and writes on the given
+// writer.
+func CaptureOutput(ctx context.Context, w io.Writer) (func(), error) {
+	outr, outw, err := os.Pipe()
+	if err != nil {
+		return nil, err
+	}
+
+	errr, errw, err := os.Pipe()
+	if err != nil {
+		outw.Close()
+		outr.Close()
+		return nil, err
+	}
+
+	cancel := func() {
+		outr.Close()
+		outw.Close()
+		errr.Close()
+		errw.Close()
+	}
+
+	os.Stdout = outw
+	os.Stderr = errw
+	mutex := sync.Mutex{}
+
+	scanOutput := func(f io.Reader) {
+		scanner := bufio.NewScanner(f)
+
+		for scanner.Scan() {
+			l := scanner.Bytes()
+
+			if scanner.Err() != nil {
+				return
+			}
+
+			mutex.Lock()
+			if _, err := w.Write(l); err != nil {
+				mutex.Unlock()
+				return
+			}
+			mutex.Unlock()
+		}
+	}
+
+	go scanOutput(outr)
+	go scanOutput(errr)
+	return cancel, nil
+}
+
+// HTTPWriter is a io.Writer that send bytes to a http server.
+type HTTPWriter struct {
+	// The url where bytes are sent.
+	URL string
+
+	// The http client used to send bytes.
+	Client *http.Client
+}
+
+func (w *HTTPWriter) Write(p []byte) (n int, err error) {
+	res, err := w.Client.Post(w.URL, "text/plain", bytes.NewBuffer(p))
+	if err != nil {
+		return 0, err
+	}
+	defer res.Body.Close()
+
+	return int(res.Request.ContentLength), nil
+}
---
+++ internal/file/std_test.go
@@ -0,0 +1,69 @@
+package file
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCaptureOutput(t *testing.T) {
+	stdout := os.Stdout
+	stderr := os.Stderr
+
+	defer func() {
+		os.Stdout = stdout
+		os.Stderr = stderr
+	}()
+
+	result := bytes.Buffer{}
+	expected := bytes.Buffer{}
+
+	cancel, err := CaptureOutput(context.Background(), &result)
+	require.NoError(t, err)
+
+	fmt.Fprintln(os.Stdout, "hello")
+	fmt.Fprintln(os.Stderr, "world")
+	fmt.Fprint(&expected, "helloworld")
+
+	time.Sleep(time.Millisecond * 10)
+	cancel()
+	require.Equal(t, expected.String(), result.String())
+}
+
+func TestHTTPWriter(t *testing.T) {
+	var wg sync.WaitGroup
+	var result []byte
+	var err error
+
+	wg.Add(1)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		result, err = ioutil.ReadAll(r.Body)
+		wg.Done()
+	}))
+	defer ts.Close()
+
+	w := &HTTPWriter{
+		URL:    ts.URL,
+		Client: http.DefaultClient,
+	}
+
+	expected := []byte(fmt.Sprint("hello"))
+	n, werr := w.Write(expected)
+	require.NoError(t, werr)
+	require.Equal(t, len(expected), n)
+
+	wg.Wait()
+
+	require.NoError(t, err)
+	require.Equal(t, expected, result)
+}
