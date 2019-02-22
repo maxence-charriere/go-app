eb19a887a92037bed7b6cf560627cd08f7faad25
Maxence Charriere <charriere@outlook.com>
2019-02-21 23:17:10 -0800

    goapp init

---
+++ cmd/goapp/init.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"io"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/pkg/errors"
+	"github.com/segmentio/conf"
+)
+
+type initConfig struct {
+	Verbose bool `conf:"v" help:"Enable verbose mode."`
+}
+
+func initProject(ctx context.Context, args []string) {
+	c := initConfig{}
+
+	ld := conf.Loader{
+		Name:    "goapp init",
+		Args:    args,
+		Usage:   "[options...] [package]",
+		Sources: []conf.Source{conf.NewEnvSource("GOAPP", os.Environ()...)},
+	}
+
+	_, args = conf.LoadWith(&c, ld)
+	pkg := "."
+	if len(args) != 0 {
+		pkg = args[0]
+	}
+
+	rootDir, err := filepath.Abs(pkg)
+	if err != nil {
+		fail("%s", err)
+	}
+
+	log("initializing project layout")
+	if err = initProjectDirectories(rootDir); err != nil {
+		fail("%s", err)
+	}
+
+	log("installing wasm_exec.js")
+	if err = installWasmExec(rootDir); err != nil {
+		fail("%s", err)
+	}
+
+	success("initialization succeeded")
+}
+
+func initProjectDirectories(rootDir string) error {
+	pkgName := filepath.Base(rootDir)
+
+	dirs := []string{
+		filepath.Join(rootDir, "cmd", pkgName+"-server"),
+		filepath.Join(rootDir, "cmd", pkgName+"-wasm"),
+		filepath.Join(rootDir, "web"),
+	}
+
+	for _, dir := range dirs {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			fail("%s", err)
+		}
+	}
+
+	return nil
+}
+
+func installWasmExec(rootDir string) error {
+	wasmExec := filepath.Join(runtime.GOROOT(), "misc", "wasm", "wasm_exec.js")
+	webWasmExec := filepath.Join(rootDir, filepath.Base(wasmExec))
+
+	src, err := os.Open(wasmExec)
+	if err != nil {
+		return errors.Wrapf(err, "opening %q failed", wasmExec)
+	}
+	defer src.Close()
+
+	dst, err := os.Create(webWasmExec)
+	if err != nil {
+		return errors.Wrapf(err, "creating %q failed", webWasmExec)
+	}
+	defer src.Close()
+
+	if _, err := io.Copy(dst, src); err != nil {
+		return errors.Wrapf(err, "copying %q failed", wasmExec)
+	}
+	return nil
+}
---
+++ cmd/goapp/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	_ "github.com/maxence-charriere/app"
+	"github.com/segmentio/conf"
+)
+
+func main() {
+	ld := conf.Loader{
+		Name: "goapp",
+		Args: os.Args[1:],
+		Commands: []conf.Command{
+			{Name: "init", Help: "Init the project layout and install the wasm dependencies."},
+			{Name: "build", Help: "Build the wasm app and its server."},
+			{Name: "run", Help: "Build and run the wasm app and its server."},
+			{Name: "clean", Help: "Delete the wasm app and its server."},
+			{Name: "help", Help: "Show the help."},
+		},
+	}
+
+	ctx, cancel := ctxWithSignals(context.Background(), os.Interrupt)
+	defer cancel()
+
+	switch cmd, args := conf.LoadWith(nil, ld); cmd {
+	case "init":
+		initProject(ctx, args)
+
+	// case "build":
+	// 	build(ctx, args)
+
+	// case "run":
+	// 	run(ctx, args)
+
+	// case "clean":
+	// 	clean(ctx, args)
+
+	case "help":
+		ld.PrintHelp(nil)
+
+	default:
+		panic("unreachable")
+	}
+}
+
+func ctxWithSignals(ctx context.Context, sigs ...os.Signal) (context.Context, func()) {
+	ctx, cancel := context.WithCancel(ctx)
+	sigc := make(chan os.Signal)
+	signal.Notify(sigc, sigs...)
+
+	go func() {
+		defer close(sigc)
+		<-sigc
+		cancel()
+	}()
+
+	return ctx, cancel
+}
