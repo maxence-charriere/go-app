c3dd317b675c908c9b161e71ff7ecddeb17b1388
Maxence Charriere <charriere@outlook.com>
2019-09-13 13:02:49 -0700

    Get current location (#291)
    * Helper function to get location url
    
    * Update app_wasm.go
    
    * Update app_wasm.go

--- pkg/app/app_other.go
+++ pkg/app/app_other.go
@@ -2,8 +2,12 @@
 
 package app
 
-func run()                              {}
-func render(Compo)                      {}
-func reload()                           {}
-func bind(msg string, c Compo) *Binding { panic("no wasm") }
-func windowSize() (w, h int)            { panic("no wasm") }
+import "net/url"
+
+func run()                                       { panic("no wasm") }
+func render(Compo)                               { panic("no wasm") }
+func reload()                                    { panic("no wasm") }
+func bind(msg string, c Compo) *Binding          { panic("no wasm") }
+func windowSize() (w, h int)                     { panic("no wasm") }
+func navigate(rawurl string, updateHistory bool) { panic("no wasm") }
+func locationURL() *url.URL                      { panic("no wasm") }
