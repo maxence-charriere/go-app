0c3d54a965968fd3ac958d67ce0a553517443894
Maxence Charriere <charriere@outlook.com>
2019-10-11 15:14:13 +0200

    Update watch.go

--- pkg/http/watch.go
+++ pkg/http/watch.go
@@ -7,6 +7,8 @@ import (
 	"github.com/maxence-charriere/app/pkg/log"
 )
 
+// Watch returns a decorated version of the given handler that provide
+// insight on the request.
 func Watch(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
@@ -14,8 +16,9 @@ func Watch(h http.Handler) http.Handler {
 		log.Info("request served").
 			T("path", r.URL.Path).
 			T("content-type", w.Header().Get("Content-Type")).
-			T("content-encoding", w.Header().Get("Content-Encoding")).
 			T("content-lenght", w.Header().Get("Content-Length")).
+			T("content-encoding", w.Header().Get("Content-Encoding")).
+			T("header", w.Header()).
 			T("duration", time.Now().Sub(start).String())
 	})
 }
