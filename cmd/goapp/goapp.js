c377151a9fe8ecc1f0b2dcc127b8daf3b5cc086d
Maxence Charriere <charriere@outlook.com>
2019-10-23 15:15:26 +0200

    Refactor goapp.js + remove unused cache when activate (#304)

--- cmd/goapp/goapp.js
+++ cmd/goapp/goapp.js
@@ -1,29 +1,50 @@
 const etag = '{{.ETag}}'
 
-self.addEventListener('install', function (e) {
+const goappCache = {
+  name: 'goapp-cache',
+  urls: [
+    {{range. Paths}}'{{.}}',
+    {{end}}'/'
+  ]
+}
+
+self.addEventListener('install', function (event) {
   console.log('intalling goapp worker', etag)
   self.skipWaiting()
 
-  e.waitUntil(
-    caches.open('goapp').then(function (cache) {
-      return cache.addAll([
-        {{range .Paths}}'{{.}}',
-        {{end}}'/'
-      ])
-    })
+  event.waitUntil(
+    caches.open(goappCache.name)
+      .then(function (cache) {
+        return cache.addAll(goappCache.urls)
+      })
   )
 })
 
-self.addEventListener('activate', event => {
-  console.log('goapp worker', etag, 'is activated')
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response
+        }
+        return fetch(event.request)
+      })
+  )
 })
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(response => {
-        return response || fetch(event.request)
+self.addEventListener('activate', function (event) {
+  console.log('goapp worker', etag, 'is activated')
+
+  const cacheWhitelist = [goappCache.name]
+
+  event.waitUntil(
+    caches.keys()
+      .then(function (cacheNames) {
+        return Promise.all(cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheNames) === -1) {
+            return caches.delete(cacheNames)
+          }
+        }))
       })
   )
 })
