597af4146741800ab1e43c4f861c1d358a63c2ec
Maxence Charriere <charriere@outlook.com>
2020-02-26 10:35:17 +0100

    refactor range

--- pkg/app/range.go
+++ pkg/app/range.go
@@ -2,36 +2,51 @@ package app
 
 import (
 	"reflect"
+	"sort"
 
 	"github.com/maxence-charriere/app/pkg/log"
 )
 
-// RangeCondition represents a control structure that iterates within a slice,
-// an array or a map.
-type RangeCondition struct {
+// RangeLoop represents a control structure that iterates within a slice, an
+// array or a map.
+type RangeLoop interface {
+	Node
+
+	nodes() []UI
+
+	// Slice sets the loop content by repeating the given function for the
+	// number of elements in the source.
+	//
+	// It panics if the range source is not a slice or an array.
+	Slice(f func(int) Node) RangeLoop
+
+	// Map sets the loop content by repeating the given function for the number
+	// of elements in the source. Elements are ordered by keys.
+	//
+	// It panics if the range source is not a map or if map keys are not strings.
+	Map(f func(string) Node) RangeLoop
+}
+
+// Range returns a range loop that iterates within the given source. Source must
+// be a slice, an array or a map with strings as keys.
+func Range(src interface{}) RangeLoop {
+	return rangeCondition{source: src}
+}
+
+type rangeCondition struct {
 	source interface{}
 	body   []UI
 }
 
-func (c RangeCondition) nodeType() reflect.Type {
+func (c rangeCondition) nodeType() reflect.Type {
 	return reflect.TypeOf(c)
 }
 
-func (c RangeCondition) nodes() []UI {
+func (c rangeCondition) nodes() []UI {
 	return c.body
 }
 
-// Range returns a condition that iterates within the given source. Source must
-// be a slice, an array or a map with strings as keys.
-func Range(src interface{}) RangeCondition {
-	return RangeCondition{source: src}
-}
-
-// Slice set the nodes of the condition by repeating the given function for
-// the amount of elements in the source.
-//
-// It panics if the range source is not a map or an array.
-func (c RangeCondition) Slice(f func(int) Node) RangeCondition {
+func (c rangeCondition) Slice(f func(int) Node) RangeLoop {
 	v := reflect.ValueOf(c.source)
 	if v.Kind() != reflect.Slice && v.Kind() != reflect.Array {
 		log.Error("range source is not a slice or array").
@@ -46,27 +61,29 @@ func (c RangeCondition) Slice(f func(int) Node) RangeCondition {
 	return c
 }
 
-// Map set the nodes of the condition by repeating the given function for the
-// amount of elements in the source.
-//
-// It panics if the range source is not a map or if map keys are not strings.
-func (c RangeCondition) Map(f func(string) Node) RangeCondition {
+func (c rangeCondition) Map(f func(string) Node) RangeLoop {
 	v := reflect.ValueOf(c.source)
 	if v.Kind() != reflect.Map {
 		log.Error("range source is not a map").
 			T("source-type", v.Type()).
 			Panic()
 	}
+	if keyType := v.Type().Key(); keyType.Kind() != reflect.String {
+		log.Error("range source keys are not strings").
+			T("key-type", keyType).
+			Panic()
+	}
 
 	c.body = nil
-	for _, key := range v.MapKeys() {
-		if key.Kind() != reflect.String {
-			log.Error("range source keys is not a string").
-				T("key-type", key.Type()).
-				Panic()
-		}
 
-		c.body = append(c.body, indirect(f(key.String()))...)
+	keys := make([]string, 0, v.Len())
+	for _, k := range v.MapKeys() {
+		keys = append(keys, k.String())
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		c.body = append(c.body, indirect(f(k))...)
 	}
 	return c
 }
