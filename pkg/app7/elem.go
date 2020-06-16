0f0d206d8fe6eb80e85f6f0aac40fdb2895abe1c
maxence-charriere <charriere@outlook.com>
2020-06-16 23:34:17 +0200

    elem refactoring: setAttr

---
+++ pkg/app7/elem.go
@@ -0,0 +1,91 @@
+package app
+
+import (
+	"context"
+	"fmt"
+)
+
+type elem struct {
+	attrs       map[string]string
+	ctx         context.Context
+	ctxCancel   func()
+	jsvalue     Value
+	parentElem  UI
+	selfClosing bool
+}
+
+func (e *elem) King() Kind {
+	return HTML
+}
+
+func (e *elem) JSValue() Value {
+	return e.jsvalue
+}
+
+func (e *elem) Mounted() bool {
+	return e.ctx != nil && e.jsvalue != nil
+}
+
+func (e *elem) parent() UI {
+	return e.parentElem
+}
+
+func (e *elem) setParent(p UI) {
+	e.parentElem = p
+}
+
+func (e *elem) appendChild(c UI) {
+	panic("not implemented")
+}
+
+func (e *elem) removeChild(c UI) {
+	panic("not implemented")
+}
+
+func (e *elem) mount() error {
+	panic("not implemented")
+}
+
+func (e *elem) update(n UI) error {
+	panic("not implemented")
+}
+
+func (e *elem) dismount() {
+	panic("not implemented")
+}
+
+func (e *elem) setAttr(k string, v interface{}) {
+	if e.attrs == nil {
+		e.attrs = make(map[string]string)
+	}
+
+	switch k {
+	case "style":
+		s := e.attrs[k] + toString(v) + ";"
+		e.attrs[k] = s
+		return
+	}
+
+	switch v := v.(type) {
+	case bool:
+		if !v {
+			delete(e.attrs, k)
+			return
+		}
+		e.attrs[k] = ""
+
+	default:
+		e.attrs[k] = toString(v)
+	}
+}
+
+type elemEventHandler struct {
+	event   string
+	jsvalue Func
+	value   EventHandler
+}
+
+func (h elemEventHandler) equals(o elemEventHandler) bool {
+	return h.event == o.event &&
+		fmt.Sprintf("%p", h.value) == fmt.Sprintf("%p", o.value)
+}
---
+++ pkg/app7/elem_test.go
@@ -0,0 +1,59 @@
+package app
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestElemSetAttr(t *testing.T) {
+	utests := []struct {
+		scenario       string
+		key            string
+		value          interface{}
+		explectedValue string
+		valueNotSet    bool
+	}{
+		{
+			scenario:       "string",
+			key:            "class",
+			value:          "test",
+			explectedValue: "test",
+		},
+		{
+			scenario:       "int",
+			key:            "max",
+			value:          42,
+			explectedValue: "42",
+		},
+		{
+			scenario:       "bool true",
+			key:            "hidden",
+			value:          true,
+			explectedValue: "",
+		},
+		{
+			scenario:    "bool false",
+			key:         "hidden",
+			value:       false,
+			valueNotSet: true,
+		},
+		{
+			scenario:       "style",
+			key:            "style",
+			value:          "margin:42",
+			explectedValue: "margin:42;",
+		},
+	}
+
+	for _, u := range utests {
+		t.Run(u.scenario, func(t *testing.T) {
+			e := &elem{}
+			e.setAttr(u.key, u.value)
+
+			v, exists := e.attrs[u.key]
+			require.Equal(t, u.explectedValue, v)
+			require.Equal(t, u.valueNotSet, !exists)
+		})
+	}
+}
