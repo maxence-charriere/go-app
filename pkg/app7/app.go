456aaf1adf2acb740eac5313b92bccd4e2476e7d
maxence-charriere <charriere@outlook.com>
2020-06-17 01:26:45 +0200

    refactor static resource function

--- pkg/app7/app.go
+++ pkg/app7/app.go
@@ -1,14 +1,46 @@
+//go:generate go run gen/html.go
+//go:generate go run gen/scripts.go
+//go:generate go fmt
+
 package app
 
-import "log"
+import (
+	"log"
+	"strings"
+)
 
 var (
 	// Logger is the logger used to log info and errors.
 	Logger func(format string, v ...interface{}) = log.Printf
+
+	remoteRootDir string
 )
 
 // EventHandler represents a function that can handle HTML events.
 type EventHandler func(ctx Context, e Event)
+
+// StaticResource makes a static resource path point to the right
+// location whether the root directory is remote or not.
+//
+// Static resources are resources located in the web directory.
+//
+// This call is used internally to resolve paths within Cite, Data, Href, Src,
+// and SrcSet. Paths already resolved are skipped.
+func StaticResource(path string) string {
+	if !strings.HasPrefix(path, "/web/") &&
+		!strings.HasPrefix(path, "web/") ||
+		remoteRootDir == "" {
+		return path
+	}
+
+	path = strings.TrimPrefix(path, "/")
+
+	if !strings.HasPrefix(path, "/") {
+		path = "/" + path
+	}
+
+	return remoteRootDir + path
+}
 
 // Window returns the JavaScript "window" object.
 func Window() BrowserWindow {
