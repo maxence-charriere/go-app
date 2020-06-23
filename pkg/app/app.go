7d75a16edd87756d4aaefb7ea9be7f4ea2d07e82
maxence-charriere <charriere@outlook.com>
2020-06-23 23:53:03 +0200

    fix nav regression

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -5,10 +5,8 @@
 package app
 
 import (
-	"fmt"
 	"net/url"
 	"strings"
-	"time"
 
 	"github.com/maxence-charriere/go-app/v6/pkg/log"
 )
@@ -62,14 +60,8 @@ func Run() {
 // Navigate navigates to the given URL.
 func Navigate(rawurl string) {
 	dispatcher(func() {
-		fmt.Println("navigate to", rawurl)
-		time.Sleep(time.Second * 2)
-
 		u, err := url.Parse(rawurl)
 		if err != nil {
-			fmt.Println("gonna panic")
-			time.Sleep(time.Second * 2)
-
 			log.Error("navigating to page failed").
 				T("url", rawurl).
 				T("error", err).
