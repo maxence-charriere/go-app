a6791296a9f30c1488c9f2a6d1d25d75b287dd18
maxence-charriere <charriere@outlook.com>
2020-06-23 08:03:54 +0200

    export dispatcher

--- pkg/app/concurrency.go
+++ pkg/app/concurrency.go
@@ -1,13 +1,13 @@
 package app
 
 var (
-	dispatch dispatcher = Dispatch
+	dispatch Dispatcher = Dispatch
 	uiChan              = make(chan func(), 512)
 )
 
-// dispatcher is a function that executes the given function on the goroutine
+// Dispatcher is a function that executes the given function on the goroutine
 // dedicated to UI.
-type dispatcher func(func())
+type Dispatcher func(func())
 
 // Dispatch executes the given function on the UI goroutine.
 func Dispatch(f func()) {
