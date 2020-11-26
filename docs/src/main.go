8101d3e350069f8d3a60a76bb9da9cc44c55d706
maxence-charriere <charriere@outlook.com>
2020-11-26 19:06:55 +0800

    add social media meta

--- docs/src/main.go
+++ docs/src/main.go
@@ -90,6 +90,10 @@ func main() {
 			
 			  gtag('config', 'G-SW4FQEM9VM');
 			</script>`,
+			`<meta property="og:url" content="https://go-app.dev">`,
+			`<meta property="og:title" content="go-app">`,
+			`<meta property="og:description" content="A package to build progressive web apps with Go programming language and WebAssembly.">`,
+			`<meta property="og:image" content="https://go-app.dev/web/images/go-app.png">`,
 		},
 		ThemeColor: backgroundColor,
 		Title:      "go-app documentation",
