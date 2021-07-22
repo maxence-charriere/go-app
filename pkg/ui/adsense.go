c6d99081157b9424a13fda786d51b1f784da99a9
maxence-charriere <charriere@outlook.com>
2021-07-23 02:49:00 +0800

    Update adsense.go

--- pkg/ui/adsense.go
+++ pkg/ui/adsense.go
@@ -138,9 +138,6 @@ func (d *adsenseDisplay) resize(ctx app.Context) {
 		ins.Get("dataset").Set("adStatus", nil)
 		d.width = w
 		d.height = h
-		if w == 0 || h == 0 {
-			return
-		}
 		fmt.Println("ad:", w, h)
 		refreshAdsenseUnits(ctx, d.Irefresh)
 	}
