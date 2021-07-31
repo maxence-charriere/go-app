980c618a2d010ae3548efe0ff81bfa4690163b46
Maxence Charriere <charriere@outlook.com>
2021-07-31 15:11:46 +0800

    refactor Adsense (#565)
    * no OnNav when fragment navigation
    
    * Update adsense.go

--- pkg/ui/adsense.go
+++ pkg/ui/adsense.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/maxence-charriere/go-app/v9/pkg/app"
-	"github.com/maxence-charriere/go-app/v9/pkg/errors"
+	"github.com/maxence-charriere/go-app/v9/pkg/logs"
 )
 
 // IAdsenseDisplay is the interface that describes a responsive Adsense display
@@ -49,8 +49,6 @@ type adsenseDisplay struct {
 	currentPath string
 	width       int
 	height      int
-	retries     int
-	loaded      bool
 }
 
 func (d *adsenseDisplay) ID(v string) IAdsenseDisplay {
@@ -80,33 +78,19 @@ func (d *adsenseDisplay) Slot(v string) IAdsenseDisplay {
 }
 
 func (d *adsenseDisplay) OnMount(ctx app.Context) {
-	ctx.Defer(d.resize)
+	ctx.Defer(d.load)
 }
 
 func (d *adsenseDisplay) OnNav(ctx app.Context) {
-	path := ctx.Page().URL().Path
-	if d.loaded && path != d.currentPath {
-		d.width = 0
-		d.height = 0
-		ctx.Defer(d.resize)
-	}
-	d.currentPath = path
+	ctx.Defer(d.load)
 }
 
 func (d *adsenseDisplay) OnResize(ctx app.Context) {
-	if d.loaded {
-		ctx.Defer(d.resize)
-	}
+	ctx.Defer(d.load)
 }
 
 func (d *adsenseDisplay) OnUpdate(ctx app.Context) {
-	if d.loaded {
-		ctx.Defer(d.resize)
-	}
-}
-
-func (d *adsenseDisplay) OnDismount() {
-	refreshAdUnits(d)
+	ctx.Defer(d.load)
 }
 
 func (d *adsenseDisplay) Render() app.UI {
@@ -115,108 +99,176 @@ func (d *adsenseDisplay) Render() app.UI {
 		ID(d.Iid).
 		Class(d.Iclass).
 		Body(
-			app.Ins().
-				ID(d.id).
-				Class("adsbygoogle").
-				Style("display", "block").
-				Style("width", "100%").
-				Style("height", "100%").
-				Style("overflow", "hidden").
-				DataSet("ad-client", d.Iclient).
-				DataSet("ad-slot", d.Islot),
+			app.Ins().ID(d.id),
 		)
 }
 
-func (d *adsenseDisplay) resize(ctx app.Context) {
-	if app.IsServer {
-		return
-	}
+func (d *adsenseDisplay) containerID() string {
+	return d.id
+}
 
+func (d *adsenseDisplay) client() string {
+	return d.Iclient
+}
+
+func (d *adsenseDisplay) slot() string {
+	return d.Islot
+}
+
+func (d *adsenseDisplay) load(ctx app.Context) {
 	ins := app.Window().GetElementByID(d.id)
 	if !ins.Truthy() {
-		app.Log(errors.New("getting adsense display ins failed").Tag("id", d.id))
 		return
 	}
 
 	layout := ins.Get("parentElement")
 	w := layout.Get("clientWidth").Int()
 	h := layout.Get("clientHeight").Int()
-	if !d.isDisplayable(w, h) {
-		d.retry(ctx)
-		return
-	}
-	d.retries = 0
 
-	if w != d.width || h != d.height {
-		ins.Set("innerHTML", "")
-		ins.Get("dataset").Set("adsbygoogleStatus", "")
-		ins.Get("dataset").Set("adStatus", "")
-		ins.Set("style", fmt.Sprintf("display:block;width:%vpx;height:%vpx;overflow:hidden", w, h))
+	currentPath := ctx.Page().URL().Path
+
+	if w != d.width || h != d.height || currentPath != d.currentPath {
 		d.width = w
 		d.height = h
-		d.loaded = true
-		refreshAdUnits(d)
+		d.currentPath = currentPath
+		ads.Push(ctx, d)
 	}
 }
 
-func (d *adsenseDisplay) isDisplayable(w, h int) bool {
-	return w >= 100 && h >= 50
+type adUnit interface {
+	app.UI
+
+	containerID() string
+	client() string
+	slot() string
 }
 
-func (d *adsenseDisplay) retry(ctx app.Context) {
-	if d.retries > 60 {
-		app.Log(errors.New("adsense display unit failed to load").
-			Tag("id", d.id).
-			Tag("width", d.width).
-			Tag("height", d.height).
-			Tag("retries", d.retries))
-		return
+type adPimp struct {
+	mutex    sync.Mutex
+	units    map[adUnit]struct{}
+	interval time.Duration
+}
+
+func newAdPimp() *adPimp {
+	return &adPimp{
+		units:    make(map[adUnit]struct{}),
+		interval: time.Millisecond * 100,
 	}
-	d.retries++
-	ctx.After(time.Second, d.resize)
-
 }
 
-var (
-	adMutex           sync.Mutex
-	adUnits           = make(map[*adsenseDisplay]struct{})
-	adRefresh         *time.Timer
-	adRefreshInterval = time.Millisecond * 100
-)
+func (p *adPimp) Push(ctx app.Context, units ...adUnit) {
+	ctx.Async(func() {
+		time.Sleep(p.interval)
 
-func refreshAdUnits(u *adsenseDisplay) {
+		p.mutex.Lock()
+		defer p.mutex.Unlock()
+
+		p.push(ctx, units...)
+	})
+}
+
+func (p *adPimp) push(ctx app.Context, units ...adUnit) {
 	if app.IsServer {
 		return
 	}
 
-	adMutex.Lock()
-	defer adMutex.Unlock()
+	for _, u := range units {
+		p.addUnit(u)
+	}
 
-	adUnits[u] = struct{}{}
+	for u := range p.units {
+		p.pushUnit(u)
+	}
 
-	if adRefresh != nil {
-		adRefresh.Reset(adRefreshInterval)
+	if len(p.units) == 0 {
 		return
 	}
 
-	adRefresh = time.AfterFunc(adRefreshInterval, func() {
-		adMutex.Lock()
-		defer adMutex.Unlock()
-
-		adsbygoogle := app.Window().Get("adsbygoogle")
-		if !adsbygoogle.Truthy() {
-			app.Log(errors.New("getting adsbygoogle failed"))
-			if adRefresh != nil {
-				adRefresh.Reset(adRefreshInterval)
-			}
-			return
-		}
-
-		for u := range adUnits {
-			if u.Mounted() {
-				adsbygoogle.Call("push", map[string]interface{}{})
-			}
-			delete(adUnits, u)
-		}
-	})
+	p.Push(ctx)
 }
+
+func (p *adPimp) addUnit(u adUnit) {
+	containerID := u.containerID()
+	if containerID == "" {
+		return
+	}
+
+	ins := app.Window().GetElementByID(containerID)
+	if !ins.Truthy() {
+		p.removeUnit(u)
+		return
+	}
+
+	for lastChild := ins.Get("lastChild"); lastChild.Truthy(); lastChild = ins.Get("lastChild") {
+		ins.Call("removeChild", lastChild)
+	}
+	ins.Set("className", "")
+	ins.Get("dataset").Set("adsbygoogleStatus", "")
+	ins.Get("dataset").Set("adStatus", "")
+	ins.Set("style", "")
+
+	p.units[u] = struct{}{}
+}
+
+func (p *adPimp) pushUnit(u adUnit) {
+	if !u.Mounted() {
+		p.removeUnit(u)
+		return
+	}
+
+	ins := app.Window().GetElementByID(u.containerID())
+	if !ins.Truthy() {
+		p.removeUnit(u)
+		return
+	}
+
+	if status := ins.Get("dataset").Get("adsbygoogleStatus").String(); status != "" {
+		p.removeUnit(u)
+		return
+	}
+
+	layout := ins.Get("parentElement")
+	w := layout.Get("clientWidth").Int()
+	h := layout.Get("clientHeight").Int()
+	if w == 0 && h == 0 {
+		app.Log(logs.New("ad unit not visible").
+			Tag("width", w).
+			Tag("height", h).
+			Tag("slot", u.slot()).
+			Tag("container-id", u.containerID()),
+		)
+		p.removeUnit(u)
+		return
+	}
+	if w*h == 0 {
+		return
+	}
+
+	adsbygoogle := app.Window().Get("adsbygoogle")
+	if !adsbygoogle.Truthy() {
+		app.Log(logs.New("adsbygoogle is not loaded"))
+		return
+	}
+
+	app.Log(logs.New("loading ad unit").
+		Tag("width", w).
+		Tag("height", h).
+		Tag("slot", u.slot()).
+		Tag("container-id", u.containerID()),
+	)
+
+	ins.Set("className", "adsbygoogle")
+	ins.Get("dataset").Set("adClient", u.client())
+	ins.Get("dataset").Set("adSlot", u.slot())
+	ins.Set("style", fmt.Sprintf("display:inline-block;width:%vpx;height:%vpx;overflow:hidden", w, h))
+
+	adsbygoogle.Call("push", map[string]interface{}{})
+}
+
+func (p *adPimp) removeUnit(u adUnit) {
+	delete(p.units, u)
+}
+
+var (
+	ads = newAdPimp()
+)
