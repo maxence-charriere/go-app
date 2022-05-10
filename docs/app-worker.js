fea850cc297f09f5daafa88d139784519f3f269b
Maxence Charriere <charriere@outlook.com>
2022-05-10 13:30:46 +0800

    update docs

--- docs/app-worker.js
+++ docs/app-worker.js
@@ -1,42 +1,26 @@
-const cacheName = "app-" + "b2a859ecd95d26cbc714ed2e7be567c42df2d460";
+const cacheName = "app-" + "5b5659f9412858d9a889d51820887845d1eff4de";
+const resourcesToCache = ["/app.css","/app.js","/manifest.webmanifest","https://storage.googleapis.com/murlok-github/icon-192.png","/wasm_exec.js","/","/web/css/prism.css","/web/css/docs.css","https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500\u0026display=swap","/web/documents/updates.md","/web/app.wasm","https://storage.googleapis.com/murlok-github/icon-512.png","/web/js/prism.js","/web/documents/what-is-go-app.md","/web/documents/home.md","/web/documents/home-next.md"];
 
-self.addEventListener("install", event => {
-  console.log("installing app worker b2a859ecd95d26cbc714ed2e7be567c42df2d460");
+self.addEventListener("install", (event) => {
+  console.log("installing app worker 5b5659f9412858d9a889d51820887845d1eff4de");
 
   event.waitUntil(
-    caches.open(cacheName).
-      then(cache => {
-        return cache.addAll([
-          "/",
-          "/app.css",
-          "/app.js",
-          "/manifest.webmanifest",
-          "/wasm_exec.js",
-          "/web/app.wasm",
-          "/web/css/docs.css",
-          "/web/css/prism.css",
-          "/web/documents/home-next.md",
-          "/web/documents/home.md",
-          "/web/documents/updates.md",
-          "/web/documents/what-is-go-app.md",
-          "/web/js/prism.js",
-          "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap",
-          "https://storage.googleapis.com/murlok-github/icon-192.png",
-          "https://storage.googleapis.com/murlok-github/icon-512.png",
-          
-        ]);
-      }).
-      then(() => {
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        return cache.addAll(resourcesToCache);
+      })
+      .then(() => {
         self.skipWaiting();
       })
   );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keyList => {
+    caches.keys().then((keyList) => {
       return Promise.all(
-        keyList.map(key => {
+        keyList.map((key) => {
           if (key !== cacheName) {
             return caches.delete(key);
           }
@@ -44,13 +28,90 @@ self.addEventListener("activate", event => {
       );
     })
   );
-  console.log("app worker b2a859ecd95d26cbc714ed2e7be567c42df2d460 is activated");
+  console.log("app worker 5b5659f9412858d9a889d51820887845d1eff4de is activated");
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
+  );
+});
+
+self.addEventListener("push", (event) => {
+  if (!event.data || !event.data.text()) {
+    return;
+  }
+
+  const notification = JSON.parse(event.data.text());
+  if (!notification) {
+    return;
+  }
+
+  const title = notification.title;
+  delete notification.title;
+
+  if (!notification.data) {
+    notification.data = {};
+  }
+  let actions = [];
+  for (let i in notification.actions) {
+    const action = notification.actions[i];
+
+    actions.push({
+      action: action.action,
+      path: action.path,
+    });
+
+    delete action.path;
+  }
+  notification.data.goapp = {
+    path: notification.path,
+    actions: actions,
+  };
+  delete notification.path;
+
+  event.waitUntil(self.registration.showNotification(title, notification));
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  const notification = event.notification;
+  let path = notification.data.goapp.path;
+
+  for (let i in notification.data.goapp.actions) {
+    const action = notification.data.goapp.actions[i];
+    if (action.action === event.action) {
+      path = action.path;
+      break;
+    }
+  }
+
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window",
+      })
+      .then((clientList) => {
+        for (var i = 0; i < clientList.length; i++) {
+          let client = clientList[i];
+          if ("focus" in client) {
+            client.focus();
+            client.postMessage({
+              goapp: {
+                type: "notification",
+                path: path,
+              },
+            });
+            return;
+          }
+        }
+
+        if (clients.openWindow) {
+          return clients.openWindow(path);
+        }
+      })
   );
 });
