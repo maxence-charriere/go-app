f28aebc16b4892746156551ef2b2ae0a69305971
Maxence Charriere <charriere@outlook.com>
2023-09-20 18:15:04 +0800

    Update html.go

--- pkg/app/gen/html.go
+++ pkg/app/gen/html.go
@@ -2134,7 +2134,7 @@ import (
 		switch t.Name {
 		case "Elem", "ElemSelfClosing":
 			fmt.Fprintf(f, `
-			// %s returns an HTML element that %s
+			// %s returns an HTML element %s
 			func %s(tag string) HTML%s {
 				e := &html%s{
 					htmlElement: htmlElement{
@@ -2156,7 +2156,7 @@ import (
 
 		default:
 			fmt.Fprintf(f, `
-			// %s returns an HTML element that %s
+			// %s returns an HTML element %s
 			func %s() HTML%s {
 				e := &html%s{
 					htmlElement: htmlElement{
