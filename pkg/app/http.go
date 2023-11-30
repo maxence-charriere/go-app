0435eacaf501703a16b7b5013a16ac9611112cb1
Maxence Charriere <charriere@outlook.com>
2023-11-30 18:04:01 +0800

    Update http.go

--- pkg/app/http.go
+++ pkg/app/http.go
@@ -552,10 +552,12 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	path := r.URL.Path
-	if strings.HasPrefix(path, "/"+h.Version+"/") {
-		path = strings.TrimPrefix(path, "/"+h.Version)
-	}
+	// path := r.URL.Path
+	// if strings.HasPrefix(path, "/"+h.Version+"/") {
+	// 	path = strings.TrimPrefix(path, "/"+h.Version)
+	// }
+
+	path := strings.TrimPrefix(r.URL.Path, "/"+h.Version)
 
 	fileHandler, isServingStaticResources := h.Resources.(http.Handler)
 	if isServingStaticResources && strings.HasPrefix(path, "/web/") {
