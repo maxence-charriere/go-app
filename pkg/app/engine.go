af72244fbd9b6cb716ef9f1be30c996a4cb77e98
Maxence Charriere <charriere@outlook.com>
2024-05-22 14:20:02 +0800

    fix app installable before wasm loading

--- pkg/app/engine.go
+++ pkg/app/engine.go
@@ -162,6 +162,7 @@ func (e *engineX) initBrowser() {
 		return
 	}
 	e.browser.HandleEvents(e.baseContext(), e.notifyComponentEvent)
+	Window().Set("goappLoaded", true)
 }
 
 func (e *engineX) notifyComponentEvent(event any) {
