67dbd42c4c96291a05779dc07a48dd8c4cc9e6f1
bruceesmith <bruceesmith@gmail.com>
2025-02-18 20:29:37 +1100

    Remove unused functions

--- docs/src/snipets/start.go
+++ docs/src/snipets/start.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
 
@@ -57,33 +56,4 @@ func main() {
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
 	}
-}
-
-type foo struct {
-	app.Compo
-
-	response []byte
-}
-
-func (f *foo) OnNav(ctx app.Context) {
-	// Launching a new goroutine:
-	ctx.Async(func() {
-		r, err := http.Get("/bar")
-		if err != nil {
-			app.Log(err)
-			return
-		}
-		defer r.Body.Close()
-
-		b, err := io.ReadAll(r.Body)
-		if err != nil {
-			app.Log(err)
-			return
-		}
-
-		// Storing HTTP response in component field:
-		ctx.Dispatch(func(app.Context) {
-			f.response = b
-		})
-	})
 }
